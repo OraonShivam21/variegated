d8c9360639b455deaf6be77c3c242947c1c572a1
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-15 16:34:13 +0530

    string to integer

---
+++ data-structures/8-string-to-integer-atoi/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if(s.length() == 0)
+            return;
+        
+        int ind = 0, sign = 1;
+        if(s.charAt(ind) == '-') {
+            sign *= -1;
+            ind++;
+        } else if(s.charAt(ind) == '+') {
+            ind++;
+        }
+
+        int result = 0;
+
+        while(ind < s.length() && Character.isDigit(s.charAt(ind))) {
+            int digit = s.charAt(ind) - '0';
+
+            if(result > (Integer.MAX_VALUE - digit) / 10)
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+
+            result = result * 10 + digit;
+            ind++;
+        }
+
+        return result * sign;
+    }
+}
