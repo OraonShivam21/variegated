8ecba3306fd954fe0f02246b98b4a5e5d1e12f6e
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-18 13:03:17 +0530

    done exercise 1

--- coding-practice/middleware-exercise/index1.js
+++ coding-practice/middleware-exercise/index1.js
@@ -1,13 +1,21 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ message: "Request successfully submitted!" });
 });
 
 app.listen(port, () => {
