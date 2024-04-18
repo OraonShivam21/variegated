f1fd7f689d05d04da57dcf802643f428773378fa
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-18 13:38:27 +0530

    done exercise 2

--- coding-practice/middleware-exercise/index2.js
+++ coding-practice/middleware-exercise/index2.js
@@ -1,10 +1,27 @@
 import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.status(200).json({ message: "Home" });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).json({ message: "About" });
+});
+
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ message: "Contact", body: req.body });
 });
 
 app.listen(port, () => {
