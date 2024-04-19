48596018ce67fc3d272176d6fb821b9ce1a0f8ac
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-19 13:04:51 +0530

    done exercise 3

--- coding-practice/middleware-exercise/index3.js
+++ coding-practice/middleware-exercise/index3.js
@@ -3,6 +3,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+  console.log("Request method and url: " + req.method + " " + req.url);
+  next();
+};
+
 app.use(logger);
 
 app.get("/", (req, res) => {
