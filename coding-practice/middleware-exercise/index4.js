fdc449f620738d0c8ebced8e887fe8ccc8e64cd3
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-19 13:19:15 +0530

    done exercise 4

--- coding-practice/middleware-exercise/index4.js
+++ coding-practice/middleware-exercise/index4.js
@@ -1,7 +1,28 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  const body = req.body;
+  const message = `
+  <h1>Your band name is:</h1>
+  <p style="font-size: 1.2rem;">${body.street}${body.pet}</p>
+  `;
+  res.send(message);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
