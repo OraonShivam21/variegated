f9e5a920fb4036f0cde7c45872c8a9d6e9812e0f
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-21 14:10:00 +0530

    done ejs partials exercise

--- coding-practice/ejs-partials/index.js
+++ coding-practice/ejs-partials/index.js
@@ -5,8 +5,25 @@ const port = 3000;
 
 // Write your code here:
 // Step 1: Render the home page "/" index.ejs
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
 // Step 2: Make sure that static files are linked to and the CSS shows up.
+
+app.use(express.static("public"));
+
 // Step 3: Add the routes to handle the render of the about and contact pages.
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
 //   Hint: Check the nav bar in the header.ejs to see the button hrefs
 // Step 4: Add the partials to the about and contact pages to show the header and footer on those pages.
 
