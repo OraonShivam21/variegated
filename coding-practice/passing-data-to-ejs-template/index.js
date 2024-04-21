6450b983a5b20db74d9a7064534a4116badf6f91
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-21 13:02:54 +0530

    done passing data to ejs template exercise

--- coding-practice/passing-data-to-ejs-template/index.js
+++ coding-practice/passing-data-to-ejs-template/index.js
@@ -6,9 +6,15 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("solution.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const { fName, lName } = req.body;
+  const numberOfLetters = fName.length + lName.length;
+  res.render("solution.ejs", { numberOfLetters });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
