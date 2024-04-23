4b57d0e92b2895a427f7f1405c68c33a0aa818ea
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-23 14:00:18 +0530

    power of x raised to n

---
+++ data-structures/50-pow-x-n/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n < 0) {
+            n = -n;
+            x = 1/x;
+        }
+
+        double power = 1;
+        while(n != 0) {
+            if((n & 1) != 0)
+                power *= x;
+            x *= x;
+            n >>>= 1;
+        }
+
+        return power;
+    }
+}
