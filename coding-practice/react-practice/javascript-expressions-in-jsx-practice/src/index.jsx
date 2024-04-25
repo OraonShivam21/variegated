d0db5aa2b0701316d0f2738a2f14fd2f34129b7b
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-25 20:49:26 +0530

    done js expression in jsx practice

--- coding-practice/react-practice/javascript-expressions-in-jsx-practice/src/index.jsx
+++ coding-practice/react-practice/javascript-expressions-in-jsx-practice/src/index.jsx
@@ -7,8 +7,16 @@
 //Created by Angela Yu.
 //Copyright 2019.
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+import React from "react";
+import ReactDOM from "react-dom";
+
+const username = "Shivam Oraon";
+const currentYear = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {username}</p>
+    <p>Copyright {currentYear}</p>
+  </div>,
+  document.getElementById("root")
+);
