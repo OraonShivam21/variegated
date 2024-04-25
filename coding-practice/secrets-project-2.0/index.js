7bb3c3835ce66b955fd10d234fa815ac8c2029d7
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-25 13:18:32 +0530

    done secrets project 2.0 exercise

--- coding-practice/secrets-project-2.0/index.js
+++ coding-practice/secrets-project-2.0/index.js
@@ -1,13 +1,35 @@
 // HINTS:
 // 1. Import express and axios
-
 // 2. Create an express app and set the port number.
-
 // 3. Use the public folder for static files.
-
 // 4. When the user goes to the home page it should render the index.ejs file.
-
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
-
 // 6. Listen on your predefined port and start the server.
+
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    const data = result.data;
+    console.log(data);
+    res.render("index.ejs", { secret: data.secret, user: data.username });
+  } catch (error) {
+    console.log(error);
+    res.render("index.ejs", {
+      secret: "",
+      user: "Some error occured while fetching random secret",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("listening on port", PORT);
+});
