d46879a4095aae92ccf3cc435e139e70569327f2
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-27 15:48:29 +0530

    done react components

---
+++ coding-practice/react-practice/react-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+const App = () => {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+};
+
+export default App;
--- coding-practice/react-practice/react-components/src/index.jsx
+++ coding-practice/react-practice/react-components/src/index.jsx
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
-import List from "./List";
+import App from "./App";
 
-ReactDOM.render(
-  <div>
-    <Heading />
-    <List />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
