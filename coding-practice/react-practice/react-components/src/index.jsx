9c209814b10bea67806b66a09a14218f2a6d5944
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-27 15:37:59 +0530

    initial commit

--- coding-practice/react-practice/react-components/src/index.jsx
+++ coding-practice/react-practice/react-components/src/index.jsx
@@ -5,16 +5,11 @@ ReactDOM.render(
   <div>
     <h1>My Favourite Foods</h1>
     <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
+      <li>Chicken Biryani</li>
+      <li>Chhole Bhature</li>
+      <li>Mushroom Chowmein</li>
     </ul>
   </div>,
   document.getElementById("root")
 );
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
