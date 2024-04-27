8c9fbc79e2fcdf40433b0c9fce2f6164dd23e2f6
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-27 20:49:52 +0530

    initial commit

--- coding-practice/react-practice/keeper-app-project/src/index.jsx
+++ coding-practice/react-practice/keeper-app-project/src/index.jsx
@@ -11,8 +11,4 @@
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
