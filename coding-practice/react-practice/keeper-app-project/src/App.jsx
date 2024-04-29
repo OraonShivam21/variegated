c988ecd31d0ba54c18d212e4fc392e8dda55d1f6
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-04-29 15:15:21 +0530

    rendered notes from notes.jsx

--- coding-practice/react-practice/keeper-app-project/src/App.jsx
+++ coding-practice/react-practice/keeper-app-project/src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./components/Header";
 import Note from "./components/Note";
 import Footer from "./components/Footer";
+import notes from "./notes";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => {
+        return (
+          <Note key={note.key} title={note.title} content={note.content} />
+        );
+      })}
       <Footer />
     </div>
   );
--- coding-practice/react-practice/keeper-app-project/src/components/Note.jsx
+++ coding-practice/react-practice/keeper-app-project/src/components/Note.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Note = () => {
+const Note = (props) => {
   return (
-    <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
-    </div>
+    <>
+      <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+      </div>
+    </>
   );
 };
 
