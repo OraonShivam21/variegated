b1d7db7da76c3f973e8b90272bb1ec335067a890
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-05-07 15:29:26 +0530

    checking if an object is an array or not

--- js-practice/index12.js
+++ js-practice/index12.js
@@ -5,3 +5,8 @@ var str = "This is a string";
 console.log("Original String:", str);
 var reverse = str.split(" ").map((word) => word.split("").reverse().join(""));
 console.log("Reversed String:", reverse.join(" "));
+
+// 2. How to check if an object is an array or not?
+var arr = [1, 2, 3, 4];
+console.log("typeof arr:", typeof arr); // will give object since arrays are considered objects in js
+console.log("Is array or not:", Array.isArray(arr)); // it will check if it is an array or not
