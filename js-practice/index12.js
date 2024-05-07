05f5c54ae57212800d0c6ce95cfe5c716e6cd5e4
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-05-07 15:34:27 +0530

    emptying an array but not resetting into new array

--- js-practice/index12.js
+++ js-practice/index12.js
@@ -10,3 +10,9 @@ console.log("Reversed String:", reverse.join(" "));
 var arr = [1, 2, 3, 4];
 console.log("typeof arr:", typeof arr); // will give object since arrays are considered objects in js
 console.log("Is array or not:", Array.isArray(arr)); // it will check if it is an array or not
+
+// 3. How to empty an array in js? But do not reset it to a new array.
+var arr2 = [12, 13, 14, 15];
+console.log("Before deleting:", arr2);
+arr2.splice(0, arr2.length);
+console.log("After deleting:", arr2);
