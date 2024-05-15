42ce24b2d32d42f9525bec0d5d806b83271967bb
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-05-15 19:39:54 +0530

    rotate image

---
+++ data-structures/48-rotate-image/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void reverse(int[] arr) {
+        int start = 0, end = arr.length - 1;
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+        }
+    }
+
+    public void rotate(int[][] matrix) {
+        // transpose
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j <= i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // reverse along column
+        for(int i = 0; i < matrix.length; i++) {
+            reverse(matrix[i]);
+        }
+    }
+}
