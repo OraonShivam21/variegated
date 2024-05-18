4d567e15d0ccf27cb8c4ee3324cd3a671a1bd58d
Shivam Oraon <138677528+OraonShivam21@users.noreply.github.com>
2024-05-18 07:47:50 +0000

    about dates index13.js

---
+++ js-practice/index13.js
@@ -0,0 +1,13 @@
+// Dates
+
+const newDate = new Date();
+console.log(newDate);
+console.log(`toDateString(): ${newDate.toDateString()}`);
+console.log(`toISOString(): ${newDate.toISOString()}`);
+console.log(`toJSON(): ${newDate.toJSON()}`);
+console.log(`toLocaleDateString(): ${newDate.toLocaleDateString()}`);
+console.log(`toLocaleString(): ${newDate.toLocaleString()}`);
+console.log(`toLocaleTimeString(): ${newDate.toLocaleTimeString()}`);
+console.log(`toString(): ${newDate.toString()}`);
+console.log(`toTimeString(): ${newDate.toTimeString()}`);
+console.log(`toUTCString(): ${newDate.toUTCString()}`);
