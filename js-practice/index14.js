85dd866d0108246002f07a16f0cce9cec37cb3c9
Shivam Oraon <138677528+OraonShivam21@users.noreply.github.com>
2024-05-20 13:41:30 +0000

    js objects

---
+++ js-practice/index14.js
@@ -0,0 +1,23 @@
+const mySym = Symbol("123");
+const marks = ["English", "Maths", "Science"];
+
+const user = {
+    name: "Shivam",
+    age: 22,
+    email: "shivam@gmail.com",
+    location: "Jaipur",
+    isLoggedIn: false,
+    [mySym]: "mySym1", // for special symbols and for accessing: user[mySym]
+    [marks]: [89, 94, 96],
+}
+
+const traverse = [
+    ...Object.keys(user),
+    ...Object.getOwnPropertySymbols(user).map(sym => sym.toString()),
+    marks.toString(),
+];
+// console.log(traverse);
+traverse.forEach(trav => {
+    console.log(`${trav}: ${user[trav]}`);
+    console.log(' ');
+})
