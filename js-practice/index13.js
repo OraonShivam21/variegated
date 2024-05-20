b88685e0e43d7baaf8c1d3608bc27d93f17515b0
Shivam Oraon <138677528+OraonShivam21@users.noreply.github.com>
2024-05-20 07:13:35 +0000

    dates

--- js-practice/index13.js
+++ js-practice/index13.js
@@ -1,13 +1,15 @@
 // Dates
 
-const newDate = new Date();
+const newDate = new Date(2024, 4, 19);
 console.log(newDate);
-console.log(`toDateString(): ${newDate.toDateString()}`);
-console.log(`toISOString(): ${newDate.toISOString()}`);
-console.log(`toJSON(): ${newDate.toJSON()}`);
-console.log(`toLocaleDateString(): ${newDate.toLocaleDateString()}`);
-console.log(`toLocaleString(): ${newDate.toLocaleString()}`);
-console.log(`toLocaleTimeString(): ${newDate.toLocaleTimeString()}`);
-console.log(`toString(): ${newDate.toString()}`);
-console.log(`toTimeString(): ${newDate.toTimeString()}`);
-console.log(`toUTCString(): ${newDate.toUTCString()}`);
+// console.log(`toDateString(): ${newDate.toDateString()}`);
+// console.log(`toISOString(): ${newDate.toISOString()}`);
+// console.log(`toJSON(): ${newDate.toJSON()}`);
+// console.log(`toLocaleDateString(): ${newDate.toLocaleDateString()}`);
+// console.log(`toLocaleString(): ${newDate.toLocaleString()}`);
+// console.log(`toLocaleTimeString(): ${newDate.toLocaleTimeString()}`);
+// console.log(`toString(): ${newDate.toString()}`);
+// console.log(`toTimeString(): ${newDate.toTimeString()}`);
+// console.log(`toUTCString(): ${newDate.toUTCString()}`);
+
+console.log(newDate.getDay());
