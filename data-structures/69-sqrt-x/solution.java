3a4edb9555df59998b5d50d47e92c6643ee07cc5
Shivam Oraon <138677528+OraonShivam21@users.noreply.github.com>
2024-05-23 13:16:37 +0000

    sqrt x

---
+++ data-structures/69-sqrt-x/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x <= 0)
+            return 0;
+        
+        int low = 1, high = x;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(mid == x / mid)
+                return mid;
+            else if(mid > x / mid)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return high;
+    }
+}
