1dfe474897a1fe23ad17d85fda6438e676a2f05d
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-06-08 21:18:17 +0530

    added bcryptjs module and added users route in server file

--- coding-practice/nodejs-mini-projects/ecommerce-api-with-rbac/server.js
+++ coding-practice/nodejs-mini-projects/ecommerce-api-with-rbac/server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
+
+const userRoute = require("./routes/user.routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/api", (req, res) => {
   res.status(200).json({ message: "Welcome to the Ecommerce API with RBAC" });
 });
+
+// all the routes
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
