a032c3e62913a073b284da613ba7e824b432aa89
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-06-13 12:28:49 +0530

    checking property descriptors in objects

--- js-practice/index14.js
+++ js-practice/index14.js
@@ -2,22 +2,33 @@ const mySym = Symbol("123");
 const marks = ["English", "Maths", "Science"];
 
 const user = {
-    name: "Shivam",
-    age: 22,
-    email: "shivam@gmail.com",
-    location: "Jaipur",
-    isLoggedIn: false,
-    [mySym]: "mySym1", // for special symbols and for accessing: user[mySym]
-    [marks]: [89, 94, 96],
-}
+  name: "Shivam",
+  age: 22,
+  email: "shivam@gmail.com",
+  location: "Jaipur",
+  isLoggedIn: false,
+  [mySym]: "mySym1", // for special symbols and for accessing: user[mySym]
+  [marks]: [89, 94, 96],
+};
 
 const traverse = [
-    ...Object.keys(user),
-    ...Object.getOwnPropertySymbols(user).map(sym => sym.toString()),
-    marks.toString(),
+  ...Object.keys(user),
+  ...Object.getOwnPropertySymbols(user).map((sym) => sym.toString()),
+  marks.toString(),
 ];
 // console.log(traverse);
-traverse.forEach(trav => {
-    console.log(`${trav}: ${user[trav]}`);
-    console.log(' ');
-})
+traverse.forEach((trav) => {
+  console.log(`${trav}: ${user[trav]}`);
+  console.log(" ");
+});
+
+const obj1 = {
+  name: "Shivam",
+  class: "X",
+  rollNo: 32,
+};
+const obj = Object.create(obj1);
+
+console.log(Object.getOwnPropertyDescriptors(obj1));
+console.log(Object.getOwnPropertyDescriptors(obj));
+// console.log(obj);
