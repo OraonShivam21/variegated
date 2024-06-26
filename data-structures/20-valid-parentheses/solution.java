7b507a661d8ed209c7dff718a98db9adce094d46
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-06-26 17:51:24 +0530

    valid parentheses

---
+++ data-structures/20-valid-parentheses/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if(stack.isEmpty())
+                    return false;
+                
+                if((c == ')' && stack.peek() == '(') || (c == ']' && stack.peek() == '[') || (c == '}' && stack.peek() == '{'))
+                    stack.pop();
+                else
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
