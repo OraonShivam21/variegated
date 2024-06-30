1fd7a148b9a6d55ee5a268a560d22cb3b87b16f2
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-06-30 16:25:08 +0530

    commonjs modules

---
+++ html-css-js-practice/modules/module2/index.js
@@ -0,0 +1,5 @@
+console.log("running index.js");
+
+const { sum } = require("./sum");
+
+console.log(sum(5, 4));
---
+++ html-css-js-practice/modules/module2/sum.js
@@ -0,0 +1,7 @@
+console.log("running sum.js");
+
+const sum = (a, b) => a + b;
+
+module.exports = { sum };
+
+console.log("below sum func");
