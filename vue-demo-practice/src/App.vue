4a525a1771b47cfb60fc8fac578682d7cb49acee
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-07-08 15:11:57 +0530

    attribute binding

--- vue-demo-practice/src/App.vue
+++ vue-demo-practice/src/App.vue
@@ -1,5 +1,5 @@
 <!-- Declarative rendering -->
-<script setup>
+<!-- <script setup>
 import { ref } from 'vue'
 
 const message = ref('Hello World')
@@ -13,4 +13,23 @@ const counter = ref({
   <h1>Make me dynamic!</h1>
   <p>{{ message }}</p>
   <p>Count is : {{ counter.count }}</p>
+</template> -->
+
+<!-- ---------------------------- -->
+
+<!-- Attribute Binding -->
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+</script>
+
+<template>
+  <h1 :class="titleClass">Make me red</h1> <!-- add dynamic class binding here -->
 </template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
