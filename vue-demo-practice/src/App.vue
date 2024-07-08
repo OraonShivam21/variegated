3a16e890d2b218c82b0b89d56dac4b8d01cfb5a8
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-07-08 15:25:45 +0530

    conditional rendering

--- vue-demo-practice/src/App.vue
+++ vue-demo-practice/src/App.vue
@@ -69,3 +69,22 @@ function onInput(e) {
    <!-- <input v-model="text" placeholder="Type here" />
   <p>{{ text }}</p>
 </template> -->
+
+<!-- ---------------------------- -->
+
+<!-- Conditional Rendering -->
+<!-- <script setup>
+import { ref } from 'vue'
+
+const awesome = ref(true)
+
+function toggle() {
+  awesome.value = !awesome.value
+}
+</script>
+
+<template>
+  <button @click="toggle">Toggle</button>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else="awesome">Oh no 😢</h1>
+</template> -->
