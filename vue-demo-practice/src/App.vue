571b6af4b059e427ff5c8447e9cf37bb07fd6804
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-07-08 15:09:32 +0530

    learning vue.js for quasar

---
+++ vue-demo-practice/src/App.vue
@@ -0,0 +1,16 @@
+<!-- Declarative rendering -->
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World')
+
+const counter = ref({
+  count: 0
+})
+</script>
+
+<template>
+  <h1>Make me dynamic!</h1>
+  <p>{{ message }}</p>
+  <p>Count is : {{ counter.count }}</p>
+</template>
